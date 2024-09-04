dc1f22d2be47498c3d16a3cf5a9a9bad61f5b562
李昕 <lixin.henry@bytedance.com>
2024-09-04 09:19:37 +0800

    chore: turn off no-unsafe-* rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -38,6 +38,8 @@ const config = {
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
     'import/order': [
       'error',
       {
