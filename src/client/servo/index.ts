91e2a62712e5b86e2497a11ca02def65dd17d469
李昕 <lixin.henry@bytedance.com>
2024-09-06 23:09:39 +0800

    refactor: rename

--- src/client/servo/index.ts
+++ src/client/servo/index.ts
@@ -4,24 +4,23 @@ import { Servo } from './servo';
 
 export * from './servo';
 
-const servos: [
-  frontLeft: Servo,
-  frontRight: Servo,
-  backLeft: Servo,
-  backRight: Servo,
-] = [
-  new Servo(FRONT_LEFT),
-  new Servo(FRONT_RIGHT),
-  new Servo(BACK_LEFT),
-  new Servo(BACK_RIGHT),
+export const frontLeftServo = new Servo(FRONT_LEFT);
+export const frontRightServo = new Servo(FRONT_RIGHT);
+export const backLeftServo = new Servo(BACK_LEFT);
+export const backRightServo = new Servo(BACK_RIGHT);
+
+export const allServos = [
+  frontLeftServo,
+  frontRightServo,
+  backLeftServo,
+  backRightServo,
 ];
-export const allServos = [...servos];
-export const frontServos = [servos[FRONT_LEFT], servos[FRONT_RIGHT]];
-export const backServos = [servos[BACK_LEFT], servos[BACK_RIGHT]];
-export const leftServos = [servos[FRONT_LEFT], servos[BACK_LEFT]];
-export const rightServos = [servos[FRONT_RIGHT], servos[BACK_RIGHT]];
-export const diagonalServos1 = [servos[FRONT_LEFT], servos[BACK_RIGHT]];
-export const diagonalServos2 = [servos[FRONT_RIGHT], servos[BACK_LEFT]];
+export const frontServos = [frontLeftServo, frontRightServo];
+export const backServos = [backLeftServo, backRightServo];
+export const leftServos = [frontLeftServo, backLeftServo];
+export const rightServos = [frontRightServo, backRightServo];
+export const diagonalServos1 = [frontLeftServo, backRightServo];
+export const diagonalServos2 = [frontRightServo, backLeftServo];
 export function updateServos() {
-  servos.forEach((servo) => servo.update());
+  allServos.forEach((servo) => servo.update());
 }
